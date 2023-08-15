53a05814e0071cbba89871386564527a06c19ebe
akhil anand sm <akhil.anand@ilearningengines.com>
2023-08-15 16:24:21 +0530

    index.js file

---
+++ index.js
@@ -0,0 +1,37 @@
+import { Express } from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+/* CONFIGUARATIONs */
+
+const __filename = fileURLToPath(import.meta.url);// to grab the fileurl
+const __dirname = path.dirname(__filename)// these two statements are required only when you add module as type in package.json
+dotenv.config();// to use .env files
+const app = Express();
+app.use(Express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}))
+app.use(morgan("common"))
+app.use(bodyParser.json({limit:"30mb", extended:true}))
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
+app.use(cors())
+app.use("/assets",express.static(path.join(__dirname,'public/assets')))//setting dir to store our images
+
+/* File Storage */
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, "public/assets")
+    },
+    filename: function(req, res, cb){
+        cb(null, file.originalname);
+    }
+})
+const upload = multer({storage})
+
+
