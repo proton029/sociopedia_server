5b97d938fa35dec979beb50877be165d465ff82c
akhil anand sm <akhil.anand@ilearningengines.com>
2023-08-16 22:56:54 +0530

    models+controllers+schema+authentication intial work only

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import {register} from './controllers/auth.js';
+
 /* CONFIGUARATIONs */
 
 const __filename = fileURLToPath(import.meta.url);// to grab the fileurl
@@ -33,6 +35,10 @@ const storage = multer.diskStorage({
     }
 })
 const upload = multer({storage})
+
+/* Routes with files */
+
+app.post("/auth/register",upload.single("picture"),register); 
 
 /* MONGOOSE */
 const PORT = process.env.PORT || 6001;
